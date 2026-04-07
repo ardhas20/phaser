6d1e2bb852d4888a7774624e97b6682165d42684
Ardita Hasmegaj <ardhas20@htl-shkoder.com>
2026-04-07 10:38:50 +0200

    Added player

--- Phaser Projekt/src/scenes/Game.js
+++ Phaser Projekt/src/scenes/Game.js
@@ -1,3 +1,5 @@
+import {Player} from '../gameObjects/Player.js'
+
 export class Game extends Phaser.Scene {
     constructor() {
         super('Game');
@@ -15,10 +17,28 @@ export class Game extends Phaser.Scene {
     this.platforms.create(600, 400, 'ground');
     this.platforms.create(50, 250, 'ground');
     this.platforms.create(750, 220, 'ground');
+
+    this.player = new Player(this,100,450);
+    this.physics.add.collider(this.player, this.platforms);
+    this.cursors = this.input.keyboard.createCursorKeys();
 }
 
-    update(time) {
+    update() {
+        if(this.cursors.left.isDown){
+            this.player.moveLeft();
+        }
+        else if(this.cursors.right.isDown){
+            this.player.moveRight();
+        }
 
+        else{
+            this.player.idle();
+        
+        }
+
+          if(this.cursors.up.isDown){
+            this.player.jump();
+        }
           
 
        
